d99bc3cc2c81d94cf762b0ef00d282038c415850
Daniel Calderon <danielcalderonuns@gmail.com>
2024-03-30 22:42:22 -0300

    remove unused library

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import countapi from 'countapi-js';
 import { ChakraProvider } from "@chakra-ui/react";
 
 import "./index.css"
 import PhoneNumberValidator from "./components/PhoneNumberValidator";
 
 const App = () => {
-  countapi.visits('global').then((result) => {
-    console.log("hi");
-    console.log(result.value);
-  });
-
   return (
     <ChakraProvider>
       <PhoneNumberValidator />
